084ce3ac0c2f6b6c7059f7370811979da8dddf01
fuuta0201 <satofuta0201@gmail.com>
2025-01-11 16:51:46 +0900

    eslint, prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["dist"], // 出力ディレクトリを無視
+  },
+  {
+    files: ["**/*.{ts,tsx}"], // TypeScript/TSXファイルを対象
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2020, // ECMAScriptバージョン
       globals: globals.browser,
+      parser: tsParser, // TypeScript用パーサー
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@typescript-eslint": tseslint,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "@typescript-eslint/no-unused-vars": ["warn"], // 未使用変数の警告
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "prettier/prettier": ["warn"], // Prettierの警告
     },
   },
-)
+];
---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // セミコロンを付ける
+  singleQuote: true, // シングルクォートを使用
+  trailingComma: "all", // 複数行で末尾カンマをつける
+  printWidth: 80, // 1行の最大文字数
+  tabWidth: 2, // インデント幅
+};
