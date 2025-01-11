ff81d5e26bfb57577f13511d5e2f66111b1e9025
fuuta0201 <satofuta0201@gmail.com>
2025-01-12 00:38:25 +0900

    feat: opening animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import "./App.css";
 import Hero from "./components/Hero";
 import What from "./components/What";
@@ -7,8 +9,59 @@ import Copyright from "./components/Copyright";
 import { ReactLenis } from "lenis/react";
 
 function App() {
+  const [showAnimation, setShowAnimation] = useState(true);
+  const [startFadeOut, setStartFadeOut] = useState(false);
+
+  useEffect(() => {
+    const fadeOutTimer = setTimeout(() => {
+      setStartFadeOut(true);
+    }, 3500);
+    const hideAnimationTimer = setTimeout(() => {
+      setShowAnimation(false);
+    }, 5000);
+
+    return () => {
+      clearTimeout(fadeOutTimer);
+      clearTimeout(hideAnimationTimer);
+    };
+  }, []);
   return (
     <ReactLenis root>
+      {showAnimation && (
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={startFadeOut ? { opacity: 0 } : {}}
+          transition={{ duration: 1.5 }}
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#128857",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            zIndex: 9999,
+          }}
+        >
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{
+              duration: 1.5,
+              delay: 0.5,
+            }}
+            style={{
+              fontSize: "2rem",
+              color: "black",
+            }}
+          >
+            Neki is beautiful.
+          </motion.h1>
+        </motion.div>
+      )}
       <main>
         <Hero />
         <What />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,19 +2,27 @@ import AnchorLink from "react-anchor-link-smooth-scroll";
 import ChevronDown from "../assets/images/icons/chevron-down.svg";
 import "../assets/styles/hero.css";
 import ScrollDown from "./ScrollDown";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero__links">
-        <AnchorLink href="#what" className="--what">
-          <span>what is Neki</span>
-          <img src={ChevronDown} alt="" />
-        </AnchorLink>
-        <AnchorLink href="#hobbies" className="--hobbies">
-          <span>Neki's hobbies</span>
-          <img src={ChevronDown} alt="" />
-        </AnchorLink>
+        <motion.div
+          className="hero__links"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 5 }}
+        >
+          <AnchorLink href="#what" className="--what">
+            <span>what is Neki</span>
+            <img src={ChevronDown} alt="" />
+          </AnchorLink>
+          <AnchorLink href="#hobbies" className="--hobbies">
+            <span>Neki's hobbies</span>
+            <img src={ChevronDown} alt="" />
+          </AnchorLink>
+        </motion.div>
       </div>
       <h1 className="hero__title">
         Neki
